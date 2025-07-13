1edb67528d2c9c3b94553978329e64d223166b19
ktkt102 <fuzisandaisuki@gmail.com>
2025-07-13 18:25:27 +0900

    Initial commit: Obsidian blog project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Obsidian自動化ブログ';
+export const SITE_DESCRIPTION = 'Obsidianの自動化と効率化に特化した情報を発信するブログです。';
