306b3bf6364b5483ccc67d60d885ad50fa3f3127
ktkt102 <fuzisandaisuki@gmail.com>
2025-07-13 23:30:56 +0900

    Trigger Netlify rebuild by adding a dummy comment

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+// This is a dummy change to trigger a Netlify rebuild.
+
 export const SITE_TITLE = 'Obsidian自動化ブログ';
 export const SITE_DESCRIPTION = 'Obsidianの自動化と効率化に特化した情報を発信するブログです。';
